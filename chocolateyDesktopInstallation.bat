764dbcba4d63f7223f3b3b4806dcb57df288d93a
Benjamin Lorentz <benjy.lorentz@gmail.com>
2020-04-22 11:01:05 +0200

    Add chocolatey desktop installation

---
+++ chocolateyDesktopInstallation.bat
@@ -0,0 +1,17 @@
+Rem Installations
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+
+choco upgrade firefox ublockorigin-firefox googlechrome thunderbird ublockorigin-chrome
+choco upgrade keepass keepass-keepasshttp
+choco upgrade keypirinha --ia "/D\=C:\Program Files\keypirinha"
+choco upgrade sharex
+choco upgrade vscodium typora pandoc
+choco upgrade mobaxterm winscp cmder
+choco upgrade discord
+choco upgrade cloudstation
+choco upgrade 7zip
+choco upgrade virtualbox virtualbox-guest-additions-guest.install
+choco upgrade etcher
